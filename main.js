32dc87f0cf5fa3b836fafbfd713b4ffb8abaa988
Kevin Cornea <kpcornea@gmail.com>
2019-01-18 22:45:53 -0800

    Initialize repository

---
+++ main.js
@@ -0,0 +1,4 @@
+// top navbar
+// jumbotron with background video play on land like codecademy intensives. name/logo/profile pic opacity and movement animation, probably slight movement upwards. after get basics down, try to implement the react to mouse thing where it moves slightly in reaction to mouse movement. not sure what to put after jumbotron, maybe projects i've made.
+
+// do i have emmett here? get if not. also hot reload. also probs react extension trav talked about if on atom. i don't think i have emmett installed
