5d317364c1d1cc396afa4d0107e45377b447158c
Kevin Cornea <kpcornea@gmail.com>
2019-01-23 23:04:47 -0800

    Update reset and page skeleton

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 // jumbotron with background video play on land like codecademy intensives. name/logo/profile pic opacity and movement animation, probably slight movement upwards. after get basics down, try to implement the react to mouse thing where it moves slightly in reaction to mouse movement. might be able to do that with vanilla JS, whichever makes more sense. not sure what to put after jumbotron, maybe projects i've made.
 
 // emmett, hot reload enabled. react extension get if necessary
+
+// review proper/conventional html class names eg jumbotron or main or what? is there a better semantic tag for main content/other areas?
+// oh yeah section tag.
+// header tag? maybe review notes i got from ashwin or whoever PM
+// separate class outside of container for title space / nav? i think so cuz that way i can get effect of never ending nav section
+// maybe use less/sass just for the upgrades like mixins etc. would be a bit easier but i also want to get a working version up asap.
+// change to slightly fatter more modern font
