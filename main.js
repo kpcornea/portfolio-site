9f24d19e39db1fc3bfed37555baa5cc1b511b4c5
Kevin Cornea <kpcornea@gmail.com>
2019-01-24 21:02:22 -0800

    Add fade animation wip

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 // separate class outside of container for title space / nav? i think so cuz that way i can get effect of never ending nav section
 // maybe use less/sass just for the upgrades like mixins etc. would be a bit easier but i also want to get a working version up asap.
 
-// change to slightly fatter more modern font
+// X change to slightly fatter more modern font. could be a bit better but good enough for now
 // from after changed bg to stars 2, from here i think Xchange bg to video bg i want, then Xupdate fonts and add animation, don't forget media queries for different screen sizes, then fancier mouse interactivity
 // dust particle vid is slightly cut off currently. overall looks pretty cool though.
 // Xedit pic of me to be circular and right size, same for github icon. probably make all links glow/at least change color on hover. an animation would be nice though.
@@ -18,3 +18,5 @@
 // might ex the nav X for now since nowhere to navigate to right now
 // maybe splash of color as glow background to pro pic, maybe also github icon
 // maybe space out items a little bit more. looking pretty good now though.
+// make dust vid stretched to full screen if possible..making a lot of trouble for me. will come back to it if time, animations more important, and it's really fine/good as is, just would be nice to cover last bottom section
+// probs cover for different browsers with animation and possibly check with multiple browsers
