03b97ae67053ec205ebc5853b0af884b165d16dd
Kevin Cornea <kpcornea@gmail.com>
2019-01-24 23:17:32 -0800

    Add media queries

--- main.js
+++ main.js
@@ -10,13 +10,16 @@
 // maybe use less/sass just for the upgrades like mixins etc. would be a bit easier but i also want to get a working version up asap.
 
 // X change to slightly fatter more modern font. could be a bit better but good enough for now
-// from after changed bg to stars 2, from here i think Xchange bg to video bg i want, then Xupdate fonts and add animation, don't forget media queries for different screen sizes, then fancier mouse interactivity
+// from after changed bg to stars 2, from here i think Xchange bg to video bg i want, then Xupdate fonts and Xadd animation, don't forget X media queries for different screen sizes, then fancier mouse interactivity if time
 // dust particle vid is slightly cut off currently. overall looks pretty cool though.
 // Xedit pic of me to be circular and right size, same for github icon. probably make all links glow/at least change color on hover. an animation would be nice though.
-// add animations on landing
+// X add animations on landing
 // maybe different font for header and links/tagline X i think fine for now
 // might ex the nav X for now since nowhere to navigate to right now
 // maybe splash of color as glow background to pro pic, maybe also github icon
 // maybe space out items a little bit more. looking pretty good now though.
 // make dust vid stretched to full screen if possible..making a lot of trouble for me. will come back to it if time, animations more important, and it's really fine/good as is, just would be nice to cover last bottom section
-// probs cover for different browsers with animation and possibly check with multiple browsers
+// probs cover for different browsers with animation and possibly check with multiple browsers, yes i think do this
+// clean up notes/code
+// X think ex the color circles for now
+// background pic looks quite scrunched on mobile. might be ok for now.
